0f1c7b9c1ffd57a591148d7204e82a63b593434e
N-Ting <1240057118@qq.com>
2020-11-28 14:51:39 +0800

    提交项目vue1

---
+++ src/components/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <el-container>
+      <!-- 头部区域 -->
+      <el-header>Header<el-button
+          type="info"
+          @click="logout"
+        >退出</el-button>
+      </el-header>
+      <!-- 页面主体区域 -->
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout () {
+      //点击logout,清除token
+      window.sessionStorage.clear()
+      //控制路由跳转到登录页面
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,40 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routes = [{
+  path: '/',
+  redirect: '/login'
+},
+{
+  path: '/login',
+  component: Login
+},
+{
+  path: '/home',
+  component: Home
+}
 ]
 
 const router = new VueRouter({
   routes
+})
+
+//挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  // to表示将要访问的路径
+  // from表示从哪里来
+  // next是下一个要做的操作
+  if (to.path === '/login') return next()
+  //获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  //token不存在,跳转登录界面
+  if (!tokenStr) return next('/login')
+  //token存在,放行
+  next()
 })
 
 export default router
