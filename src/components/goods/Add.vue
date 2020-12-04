ee318f5af5f0237d6fe77cce468c61b53b561aef
N-Ting <1240057118@qq.com>
2020-12-04 20:04:34 +0800

    提交第二次goods_list

--- src/components/goods/Add.vue
+++ src/components/goods/Add.vue
@@ -56,7 +56,7 @@
               label="商品名称"
               prop="goods_name"
             >
-              <el-input></el-input>
+              <el-input v-model="addForm.goods_name"></el-input>
             </el-form-item>
             <el-form-item
               label="商品价格"
@@ -135,19 +135,62 @@
           <el-tab-pane
             label="商品图片"
             name="3"
-          > </el-tab-pane>
+          >
+            <!-- 图片上传区域 -->
+            <!-- action要上传到那个地址的那个后台接口 -->
+            <!-- on-preview点击名称时处理预览的事件 -->
+            <!-- on-remove移出文件时触发的事件 -->
+            <!-- list-type文件列表呈现的方式 -->
+            <!-- headers设置上传的请求头 -->
+            <el-upload
+              :action="uploadURL"
+              :on-preview="handlePreview"
+              :on-remove="handleRemove"
+              list-type="picture"
+              :headers="headerObj"
+              :on-success='handleSuccess'
+            >
+              <el-button
+                size="small"
+                type="primary"
+              >点击上传</el-button>
+            </el-upload>
+          </el-tab-pane>
           <el-tab-pane
             label="商品内容"
             name="4"
-          > </el-tab-pane>
+          >
+            <!-- 副文本编译器组件 v-model将里面的内容双向绑定到data中-->
+            <quill-editor v-model="addForm.goods_introduce">
+            </quill-editor>
+            <el-button
+              type="primary"
+              class="btnAdd"
+              @click="addGood"
+            >添加商品</el-button>
+          </el-tab-pane>
         </el-tabs>
       </el-form>
     </el-card>
-
+    <!-- 图片预览对话框 -->
+    <el-dialog
+      title="图片预览"
+      :visible.sync="previewVisible"
+      width="50%"
+    >
+      <img
+        :src="previewPath"
+        alt=""
+        class="previewImg"
+      >
+    </el-dialog>
   </div>
 </template>
 
 <script>
+//在行为之前导入lodash
+import _ from 'lodash'
+
 export default {
   data () {
     return {
@@ -160,7 +203,13 @@ export default {
         goods_weight: 0,
         goods_number: 0,
         //级联选择器的选中项保存的数组
-        goods_cat: []
+        goods_cat: [],
+        //图片临时路径
+        pics: [],
+        // 商品的详情描述
+        goods_introduce: '',
+        //包含动态参数和静态属性的数组
+        attrs: []
       },
       //   校验规则对象
       addFormRules: {
@@ -189,7 +238,18 @@ export default {
       //动态参数保存的数组
       manyTableData: [],
       //静态属性列表数据
-      onlyTableData: []
+      onlyTableData: [],
+      //上传图片的地址
+      uploadURL: 'http://127.0.0.1:8888/api/private/v1/upload',
+      //图片上传请求头组件的对象
+      headerObj: {
+        //添加token验证的Authorization字段
+        Authorization: window.sessionStorage.getItem("token")
+      },
+      //保存图片真实的url地址
+      previewPath: '',
+      //图片预览对话框默认不显示
+      previewVisible: false
     }
   },
   created () {
@@ -226,7 +286,9 @@ export default {
       //   return false
       //判断oldActiveName是否等于0，并且商品分类选择的时第3项
       if (oldActiveName === '0' && this.addForm.goods_cat.length !== 3) {
+        //提示要选择商品分类
         this.$message.error('请先选择商品分类')
+        //并且返回false，阻止切换
         return false
       }
     },
@@ -260,11 +322,87 @@ export default {
         if (res.meta.status !== 200) return this.$message.error('获取静态属性失败')
         //成功就将数据保存到data里的onlyData数组里
         this.onlyTableData = res.data
-        console.log(this.onlyTableData);
+        // console.log(this.onlyTableData);
       }
 
     },
+    //处理图片预览效果的事件
+    handlePreview (file) {
+      // console.log(file);
+      this.previewPath = file.response.data.url
+      // console.log(this.previewPath);
+      //点击图片名称时显示对话框
+      this.previewVisible = true
+    },
+    //处理图片移除事件，可以得到要删除的那个文件列表的信息
+    handleRemove (file) {
+      // console.log(file);
+      //获取将要删除图片的临时路径
+      const filePath = file.response.data.tmp_path
+      //从pics数组找到这个图片对应的索引值
+      const index = this.addForm.pics.findIndex(item => item.pic === filePath)
+      //调用数组的splice方法，把图片信息对象从pics数组删除
+      this.addForm.pics.splice(index, 1)
+      // console.log(this.addForm);
+    },
+    //监听图片上传成功的事件,可以拿到服务器返回的数据
+    handleSuccess (response) {
+      // console.log(response);
+      //拼接一个图片信息对象
+      const picInfo = { pic: response.data.tmp_path }
+      //将返回来的数据push到data里的addForm里的pics数组里
+      this.addForm.pics.push(picInfo)
+      // console.log(this.addForm);
+    },
+    // 点击添加商品时触发的函数
+    addGood () {
+      // console.log(this.addForm);
+      // 对表单数据进行预校验
+      this.$refs.addFormRef.validate(async valid => {
+        // 数据不合法，valid=false，返回
+        if (!valid) return this.$message.error('请填写必要的表单项！')
+        //因为后台需要的goods_cat是以,分割的字符串,而这里的goods_cat是一个数组
+        // 直接进行join的话会报错，因为级联的双向绑定是数组
+        //先需要对addForm表单进行深拷贝
+        const form = _.cloneDeep(this.addForm)
+        form.goods_cat = form.goods_cat.join(',')
 
+        //处理动态参数
+        // 循环遍历数组中的每一项，并且以一个对象的形式存储需要的attr_id和attr_value
+        //因为attr_value需要的是一个字符串，而attr_vals是一个数组，所以需要join
+        this.manyTableData.forEach(item => {
+          const newInfo = {
+            attr_id: item.attr_id,
+            attr_value: item.attr_vals.join(' ')
+          }
+          //将newInfo添加进addForm上的attrs数组上
+          this.addForm.attrs.push(newInfo)
+        })
+
+        //处理静态属性
+        this.onlyTableData.forEach(item => {
+          const newInfo = {
+            attr_id: item.attr_id,
+            attr_value: item.attr_vals
+          }
+          //将newInfo添加进addForm上的attrs数组上
+          this.addForm.attrs.push(newInfo)
+        })
+        //因为我们向后台传递的是form对象，所以需要将addForm上的attrs数组赋值给form上的attrs数组
+        form.attrs = this.addForm.attrs
+        // console.log(form);
+
+        //发送post请求添加商品
+        const { data: res } = await this.$http.post('goods', form)
+        //如果发送请求失败
+        if (res.meta.status !== 201) return this.$message.error('添加商品失败！')
+
+        this.$message.success('添加商品成功！')
+        // console.log(res.data);
+        //通过编程式导航跳转到商品列表页面
+        this.$router.push('/goods')
+      })
+    }
   },
   //计算属性
   computed: {
@@ -280,5 +418,11 @@ export default {
 <style lang="less" scoped>
 .el-checkbox {
   margin: 0 10px 0 0 !important;
+}
+.previewImg {
+  width: 100%;
+}
+.btnAdd {
+  margin-top: 15px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -10,6 +10,15 @@ import './assets/css/global.css'
 import axios from 'axios'
 //导入第三方插件vue-table-with-tree-gird
 import TableTree from 'vue-table-with-tree-grid'
+//导入副文本编译器
+import VueQuillEditor from 'vue-quill-editor'
+//导入编译器的样式
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
+
+
+
 // 初始化全局url
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 //请求在到达服务器之前,先会调用use中的这个回调函数来添加请求头信息
@@ -33,6 +42,8 @@ Vue.prototype.$http = axios
 Vue.config.productionTip = false
 //注册为全局组件
 Vue.component('tree-table', TableTree)
+//将副文本编译器注册为全局可用的组件
+Vue.use(VueQuillEditor)
 //注册一个全局时间过滤器,传入一个时间参数
 Vue.filter('dateFormat', function (originVal) {
   //根据给定的时间创建一个时间对象
